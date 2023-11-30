8bb2f46ce62ca438acd39eb35e6cd3f15765d796
Nicola Sorace <sorace.n@gmail.com>
2023-11-30 17:16:47 +0000

    Various changes
    Notably, Hyprland now imports a `device.conf` file into the main
    configuration, allowing for per-device settings.

---
+++ hypr/scripts/lock.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+swaylock -p -i /home/nicola/Pictures/wallpapers/jwst_carina_blur.png\
+    --indicator-radius 80\
+    --inside-color 00000020\
+    --ring-color 00000020\
+    --line-color 00000000\
+    --key-hl-color ffffff50\
+    --bs-hl-color b72240ff\
+    --separator-color 00000000\
+    --inside-ver-color ffffff50\
+    --ring-ver-color ffffff50\
+    --line-ver-color 00000000\
+    --text-ver-color 00000090\
+    --inside-wrong-color 00000095\
+    --ring-wrong-color 00000095\
+    --line-wrong-color 00000000\
+    --text-wrong-color b72240ff\
+    --inside-clear-color 00000095\
+    --ring-clear-color 00000095\
+    --line-clear-color 00000000\
+    --text-clear-color ffffff70\
+
--- sway/scripts/window_select.sh
+++ sway/scripts/window_select.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp -b 0D1724aa -c 268bd2 -d
+/home/nicola/.config/hypr/scripts/window_select.sh
