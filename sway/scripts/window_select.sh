bed989310058ebf7f9f638311d599b909e120455
Nicola Sorace <sorace.n@gmail.com>
2023-12-01 18:29:32 +0000

    Various changes
    - Added toolbar widget tooltips
    - Added toolbar systray (Not merged yet! Needs custom `eww` build)
    - Improved `Hyprland` decorations
    - Use `grimblast` for screenshots instead of custom scripts
    - Made some windows float by default
    - Minor `mako` improvements
    - Added `qt6ct` for uniform theming (also using `nwg-look`)

--- sway/scripts/window_select.sh
+++ sway/scripts/window_select.sh
@@ -1,1 +1,2 @@
-/home/nicola/.config/hypr/scripts/window_select.sh
+#!/bin/bash
+swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp -b 0D1724aa -c 268bd2 -d
